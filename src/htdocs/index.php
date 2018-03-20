0c7364b7fcbd9ae965141a8d1a15ffe7c7f641c5
Aleksey Filatev <afilatev@plesk.com>
2018-03-20 12:11:25 +0100

    Up minimal Plesk version to 17.8.11 and remove usless context initialization from module entry point

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,6 +1,4 @@
 <?php
 // Copyright 1999-2017. Plesk International GmbH. All rights reserved.
 
-$moduleId = basename(__DIR__);
-pm_Context::init($moduleId);
 (new \PleskExt\ExtExampleUILibrary\Application())->run();
